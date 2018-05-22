2cae88ee946bfad08cd3eac65083661b77823f35
user <regwebghost@yandex.ru>
2018-05-22 20:24:21 +0700

    remove db name from connect

--- connect.sh
+++ connect.sh
@@ -2,4 +2,4 @@
 # Connect to mysql from docker-compose
 set -e
 
-mysql -h localhost -P 3306 --protocol=tcp -uroot --default-character-set=utf8 -prootpass depo
+mysql -h localhost -P 3306 --protocol=tcp -uroot --default-character-set=utf8 -prootpass
