8fdd2aedd372f9e28c819cfa4720501179e0d103
user <regwebghost@yandex.ru>
2018-05-23 18:02:32 +0700

    update 5 query

--- queries/5.sql
+++ queries/5.sql
@@ -4,15 +4,34 @@
 -- Вывод: Предыдущий остановочный пункт, Текущий остановочный пункт, Следующий 
 -- остановочный пункт
 
-SELECT IFNULL( (SELECT field1 FROM table WHERE id = 123 LIMIT 1) ,'not found');
-
 SET @curr_id = 3;
+SET @undefined_val = 'Отсутствует';
 
-SELECT stop_point.id, stop_point.name FROM stop_point
-where stop_point.id = @curr_id
-      OR stop_point.id = (
-        SELECT MAX(stop_point.id) FROM stop_point 
+-- Base
+SELECT 
+    IFNULL (
+    (
+    SELECT stop_point.name FROM stop_point
+    WHERE stop_point.id = (
+        SELECT MAX(stop_point.id) 
+        FROM stop_point 
         WHERE stop_point.id < @curr_id)
-      OR stop_point.id = (
-        SELECT MIN(stop_point.id) FROM stop_point 
-        WHERE stop_point.id > @curr_id)
+    ), 
+    @undefined_val), 
+
+    IFNULL (
+    (
+        SELECT stop_point.name FROM stop_point
+        WHERE stop_point.id = @curr_id
+    ), 
+    @undefined_val),
+
+    IFNULL (
+    (
+        SELECT stop_point.name FROM stop_point
+        WHERE stop_point.id = (
+            SELECT MIN(stop_point.id) 
+            FROM stop_point 
+            WHERE stop_point.id > @curr_id)
+    ), 
+    @undefined_val)
