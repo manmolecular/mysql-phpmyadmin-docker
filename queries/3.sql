703b1ecc4606ba20d4b803ef504f307d69cde7c2
user <regwebghost@yandex.ru>
2018-05-23 15:32:59 +0700

    add 3 query

---
+++ queries/3.sql
@@ -0,0 +1,12 @@
+-- 3. Найти маршрут у которых больше всего остановок. Если таких маршрутов 
+-- несколько, то вывести один, любой.*+Подзапросы использовать нельзя.+*
+-- Вывод: Номер маршрута
+
+SELECT route.num, count(schedule.stop_point_id) FROM route
+    INNER JOIN schedule ON schedule.route_id = route.id
+
+WHERE schedule.route_id != 0
+
+GROUP BY route.num
+ORDER BY count(schedule.stop_point_id) DESC
+LIMIT 1
