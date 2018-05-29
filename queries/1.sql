8254a986a4616d1d37599353f6adb95f59cf20c9
user <regwebghost@yandex.ru>
2018-05-29 09:21:59 +0700

    add distinct

--- queries/1.sql
+++ queries/1.sql
@@ -4,13 +4,11 @@
 -- в) Работают с кассиром-кондуктором на одном автобусе, причём 
 -- кассир-кондуктор из другого Транспортного депо.
 
-SELECT bus_driver.name FROM bus_driver
+SELECT DISTINCT bus_driver.name FROM bus_driver
     INNER JOIN bus ON bus.driver_id = bus_driver.id
     INNER JOIN bus_checker ON bus_checker.id = bus.checker_id
 
 WHERE bus_driver.sign_date <= (curdate() - interval 1 year)
-
-GROUP BY bus_driver.id
 
 HAVING  SUM(bus.num_region = 70) > 0 AND
         SUM(bus_driver.trans_depo_id != bus_checker.trans_depo_id) > 0;
